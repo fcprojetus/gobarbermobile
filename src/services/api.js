25ed5cd2d09859dc122d73ac56bb70114df5192f
Filipe Costa <fcprojetus@gmail.com>
2020-05-06 10:26:13 -0300

    Deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://10.0.2.2:3333',
+  baseURL: 'http://167.71.188.136',
+  baseURL_Local: 'http://10.0.2.2:3333',
 });
 
 export default api;
